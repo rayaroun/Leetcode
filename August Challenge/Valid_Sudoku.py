1558c139a49ee3ac8699bc55e458f2b67c2dcb42
rayaroun <45216924+rayaroun@users.noreply.github.com>
2021-08-20 21:20:43 -0700

    Update Valid_Sudoku.py

--- August Challenge/Valid_Sudoku.py
+++ August Challenge/Valid_Sudoku.py
@@ -4,7 +4,7 @@
 '''
 
 
-etermine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
     Each row must contain the digits 1-9 without repetition.
     Each column must contain the digits 1-9 without repetition.
