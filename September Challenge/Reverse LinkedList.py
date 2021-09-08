2fb77ec054aaf8fbe757dc837300368e6b8f0c16
Aroun <45216924+rayaroun@users.noreply.github.com>
2021-09-07 23:23:22 -0700

    Create Reverse LinkedList.py

---
+++ September Challenge/Reverse LinkedList.py
@@ -0,0 +1,68 @@
+'''
+
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is the range [0, 5000].
+    -5000 <= Node.val <= 5000
+
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+        
+        
+def reversey(head):
+
+    # If head is empty or has reached the list end
+    if head is None or head.next is None:
+        return head
+
+    # Reverse the rest list
+    rest = reversey(head.next)
+
+    # Put first element at the end
+    head.next.next = head
+    head.next = None
+
+    # Fix the header pointer
+    return rest
+# Function to reverse the linked list
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return reversey(head)
+        
+   
+        
+        
