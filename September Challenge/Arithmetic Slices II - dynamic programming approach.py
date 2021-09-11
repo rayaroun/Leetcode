69e53f06ef5c9dfb05b2cd4ca5aec16bc8d6ac87
Aroun <45216924+rayaroun@users.noreply.github.com>
2021-09-10 23:51:19 -0700

    Create Arithmetic Slices II - dynamic programming approach.py

---
+++ September Challenge/Arithmetic Slices II - dynamic programming approach.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numberOfArithmeticSlices(self, nums):
+        from collections import defaultdict
+        ans = 0
+        dp = [defaultdict(int) for item in nums]
+        for i in range(len(nums)):
+            for j in range(i):
+                diff = nums[i] - nums[j]
+                dp[i][diff] += 1
+                if diff in dp[j]: # Check Key
+                    dp[i][diff] += dp[j][diff]
+                    ans += dp[j][diff]
+        return ans
